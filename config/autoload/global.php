04c2bdc6bda0fdd45c0791e4a3eee87b355e702d
wangkang <wkang@vip.deyi.com>
2016-02-26 18:47:07 +0800

    修改pdo数据库配置

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,13 @@
  * file.
  */
 
-return array(
-    // ...
-);
+return [
+    'db' => [
+        'driver' => 'Pdo',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4;',
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_STRINGIFY_FETCHES => false,
+        ],
+    ],
+];
