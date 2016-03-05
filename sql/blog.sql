bc8b7ddab524be2241c316eae379d7d89f72ffe0
wangkang <wkang@vip.deyi.com>
2016-03-05 11:13:50 +0800

    修改数据表

--- sql/blog.sql
+++ sql/blog.sql
@@ -44,7 +44,227 @@ CREATE TABLE `blog_admin_user` (
 
 LOCK TABLES `blog_admin_user` WRITE;
 /*!40000 ALTER TABLE `blog_admin_user` DISABLE KEYS */;
+INSERT INTO `blog_admin_user` VALUES (1,'wangkang','434911688babdfa00f0a70a6c9aa013c');
 /*!40000 ALTER TABLE `blog_admin_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog_blog`
+--
+
+DROP TABLE IF EXISTS `blog_blog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_blog` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(180) NOT NULL DEFAULT '',
+  `create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `content` longtext,
+  `views` int(11) NOT NULL DEFAULT '0',
+  `thumbnail` varchar(255) NOT NULL DEFAULT '',
+  `author` varchar(180) NOT NULL DEFAULT '',
+  `typeid` int(11) NOT NULL DEFAULT '0',
+  `comnum` int(11) NOT NULL DEFAULT '0',
+  `is_top` tinyint(1) NOT NULL DEFAULT '0',
+  `tagids` varchar(8192) NOT NULL DEFAULT '',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_blog`
+--
+
+LOCK TABLES `blog_blog` WRITE;
+/*!40000 ALTER TABLE `blog_blog` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_blog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog_cate`
+--
+
+DROP TABLE IF EXISTS `blog_cate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_cate` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(180) NOT NULL DEFAULT '',
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `desc` varchar(255) NOT NULL DEFAULT '',
+  `sortid` int(11) NOT NULL DEFAULT '0',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_cate`
+--
+
+LOCK TABLES `blog_cate` WRITE;
+/*!40000 ALTER TABLE `blog_cate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_cate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog_comment`
+--
+
+DROP TABLE IF EXISTS `blog_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_comment` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `blog_id` int(11) NOT NULL DEFAULT '0',
+  `poster` varchar(180) NOT NULL DEFAULT '',
+  `comment` text,
+  `mail` varchar(60) NOT NULL DEFAULT '',
+  `create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `ip` varchar(128) NOT NULL DEFAULT '',
+  `url` varchar(75) NOT NULL DEFAULT '',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_comment`
+--
+
+LOCK TABLES `blog_comment` WRITE;
+/*!40000 ALTER TABLE `blog_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog_link`
+--
+
+DROP TABLE IF EXISTS `blog_link`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_link` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(180) NOT NULL DEFAULT '',
+  `url` varchar(180) NOT NULL DEFAULT '',
+  `desc` varchar(255) NOT NULL DEFAULT '',
+  `sortid` int(10) unsigned NOT NULL DEFAULT '0',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_link`
+--
+
+LOCK TABLES `blog_link` WRITE;
+/*!40000 ALTER TABLE `blog_link` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_link` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog_navi`
+--
+
+DROP TABLE IF EXISTS `blog_navi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_navi` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(180) NOT NULL DEFAULT '',
+  `url` varchar(180) NOT NULL DEFAULT '',
+  `pid` int(11) NOT NULL DEFAULT '0',
+  `sortid` int(11) NOT NULL DEFAULT '0',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_navi`
+--
+
+LOCK TABLES `blog_navi` WRITE;
+/*!40000 ALTER TABLE `blog_navi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_navi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog_options`
+--
+
+DROP TABLE IF EXISTS `blog_options`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_options` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(180) NOT NULL DEFAULT '',
+  `value` longtext,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_options`
+--
+
+LOCK TABLES `blog_options` WRITE;
+/*!40000 ALTER TABLE `blog_options` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_options` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog_tag`
+--
+
+DROP TABLE IF EXISTS `blog_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_tag` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(180) NOT NULL DEFAULT '',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_tag`
+--
+
+LOCK TABLES `blog_tag` WRITE;
+/*!40000 ALTER TABLE `blog_tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog_uploads`
+--
+
+DROP TABLE IF EXISTS `blog_uploads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_uploads` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(180) NOT NULL DEFAULT '',
+  `blog_id` int(11) NOT NULL DEFAULT '0',
+  `create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_uploads`
+--
+
+LOCK TABLES `blog_uploads` WRITE;
+/*!40000 ALTER TABLE `blog_uploads` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_uploads` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -56,4 +276,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-04 14:29:32
+-- Dump completed on 2016-03-05 11:13:08
