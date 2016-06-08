2cd6d41f8041062cbb41292aeee545a5416caf97
wkang <wkang@vip.deyi.com>
2016-06-08 10:24:06 +0800

    添加博客list接口

--- module/V1/src/V1/Controller/BlogController.php
+++ module/V1/src/V1/Controller/BlogController.php
@@ -3,6 +3,9 @@
 namespace V1\Controller;
 
 use Application\Common\V1BaseController;
+use Zend\Db\Sql\Expression;
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\Where;
 
 class BlogController extends V1BaseController
 {
@@ -25,5 +28,29 @@ class BlogController extends V1BaseController
         $rpp = $rpp > 0 ? $rpp : 10;
         $page = $page > 0 ? --$page : 0;
         $offset = $rpp * $page;
+        $where = new Where();
+        $tBlog = $this->getTable('blog_blog');
+        $total_num = $tBlog->select(
+            function (Select $select) use ($where) {
+                $select->columns(['total_num' => new Expression('count(id)')])
+                    ->where($where);
+            })->current()->offsetGet('total_num');
+        $total_num = $total_num ?: '0';
+        $data = $tBlog->select(
+            function (Select $select) use ($where, $offset, $rpp) {
+                $select->columns(['*'])
+                    ->join(
+                        ['a' => 'blog_cate'],
+                        'a.id=blog_blog.cate_id',
+                        ['cate_title' => 'title'],
+                        $select::JOIN_LEFT
+                    )->where($where)->offset($offset)->limit($rpp);
+            })->toArray();
+        $data = [
+            'data' => $data,
+            'total_num' => $total_num
+        ];
+        echo self::jsonEncode($data);
+        return false;
     }
 }
